b9891cb6cce9c15cdf8c72a59e907629655faff6
jessicaliu6251 <jiaxinjessica.liu@mail.utoronto.ca>
2019-01-16 21:28:43 -0500

    can receive text(string) and audio(saved as file, using datetime as filenames, need to insert algorithm call

--- pythonServer.py
+++ pythonServer.py
@@ -1,13 +1,38 @@
 import socket               # Import socket module
+import datetime
 
 s = socket.socket()         # Create a socket object
-host = '100.65.196.219' # Get local machine name
+host = '192.168.1.109' # Get local machine name
 port = 5000                # Reserve a port for your service.
 s.bind((host, port))        # Bind to the port
 print(host)
 s.listen(5)                 # Now wait for client connection.
+
+
 while True:
-   c, addr = s.accept()     # Establish connection with client.
-   print ('Got connection from', addr)
-   c.send(b'Thank you for connecting')
-   c.close()                # Close the connection
+    c, address = s.accept()  # Establish connection with client.
+    print('Got connection from', address)  # run algorithm
+    data = c.recv(1024)
+    c.send(b'1')
+    if data:
+        print("receiving text", data)
+        text = c.recv(1024)
+        print(text)
+        c.send(b'1')
+
+    data = c.recv(1024)
+    c.send(b'1')
+    if data:
+        print("receiving audio", data)
+        audio = c.recv(1024)
+        fileName = datetime.datetime.now().strftime("%I_%M_%B_%d_%Y")
+        audioFile = open(fileName + ".wav", 'wb')
+        while audio:
+            audioFile.write(audio)
+            audio = c.recv(1024)
+        print("done receiving")
+        audioFile.close()
+    c.send(b'Done receiving') #send label back
+    #call algorithm
+    #c.send(b'1') #send label
+    c.close()                           # Close the connection
